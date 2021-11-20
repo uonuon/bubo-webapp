1dd4dda6bde7761af209a2de0e4a7a0aa157705c
Mina Fayek <minaf65@gmail.com>
2021-11-20 03:47:01 +0200

    Updated modules

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,12 @@
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config
-}
+const { override, addBabelPlugin } = require('customize-cra')
+module.exports = override(
+  addBabelPlugin([
+    '@babel/plugin-proposal-optional-chaining',
+    { loose: false },
+  ]),
+  addBabelPlugin([
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    { loose: false },
+  ]),
+  addBabelPlugin(['@babel/plugin-syntax-bigint', { loose: false }])
+)
